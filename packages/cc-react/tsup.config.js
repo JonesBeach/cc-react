bb3e3a1de708ba031ae8805c6bdd186dab97050f
CCleanerShot <31040856+CCleanerShot@users.noreply.github.com>
2025-10-21 14:19:34 -0800

    misc: removed tsup for redundancy

--- packages/cc-react/tsup.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup";
-export default defineConfig({
-    clean: true,
-    dts: true,
-    entry: ["./src/packages/cc-react/index.ts"],
-    format: ["cjs", "esm"],
-    shims: true,
-    skipNodeModulesBundle: true,
-});
